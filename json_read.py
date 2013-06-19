98829d5730dfd27b859f3c9a810aaf7332adc77f
gordonyen <gordonyen@gmail.com>
2013-06-19 00:54:29 -0400

    Update json_read.py
    add comments and the function of the get_src_ip

--- json_read.py
+++ json_read.py
@@ -1,7 +1,7 @@
 #call library
 
 import json
-import pprint
+from pprint import pprint
 from netaddr import *
 
 json_data = open('example.json')
@@ -12,15 +12,21 @@ json_object = json.load(json_data)
 
 
 # read network_layer from object
-#def get_ip():
-json_ip = json_object["address"]["src"]["network_layer"]["ip_address"]
-ip = json.dumps(json_ip, separators=(', ',': '))
-for key, value in json_ip.iteritems():
-  print(key, value)
-
-
-  #define it is ipv4 or ipv6
-
+def get_src_ip(event):
+  json_ip = json_object['address']['src']['network_layer']['ip_address']
+  #ip = json.dumps(json_ip, sort_keys=True, separators=(', ',': '))
+  for key, value in json_ip.iteritems():
+    print(key,value)
+  #check the format is ipv4, ipv6 or else
+  #take out the ips that are ipv4
+  #take out the ips taht are ipv6
+  #emerge them into a new dict.
+  #then the user can use get_src_ip(ipv4) to get ipv4
+  #or get_src_ip(ipv6) to get ipv6
+  
+  
+  
+  return key, value
 
 
 # read data_link_layer from object 
