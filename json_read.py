b23d182dd56f0431ab5535c7aa782bce41cb1067
gordonyen <gordonyen@gmail.com>
2013-06-20 17:48:54 -0300

    update json_read.py
    added check ip

--- json_read.py
+++ json_read.py
@@ -6,58 +6,58 @@ import json
 from pox.lib.addresses import IPAddr, EthAddr
 from netaddr import *
 from pox.lib.addresses import IPAddr, EthAddr
+import socket
 
-def load_json(name):
-  json_data = open(name)
-  json_object = json.load(json_data) #type: dict. 
-  return json_object, json_data
-
-
-# read transport_layer from object
-
-
-# read network_layer from object
-def get_src_ip(event):
-  json_ip = json_object['address']['src']['network_layer']['ip_address']
-  #ip = json.dumps(json_ip, sort_keys=True, separators=(', ',': '))
-  for key, value in json_ip.iteritems():
-    #key = key # todo: find a way to link key, but is key needed?
-    ip = IPAddr(value)
-    return ip
-  for ipnet in ip:
-    parts = ipnet.split("/")
-    ip = parts[0]
-    bits = int(parts[1]) if len(parts)>1 else 32
-    # set the IP address
-    setattr(m, attr, ipnet)
-
-    # gets converted to just the ip address during query
-    self.assertEqual(getattr(m, attr), ip)
-
-    # the get_#{attr} method gives a tuple of (ip, cidr-bits)
-    self.assertEqual( getattr(m, "get_"+attr)(), (ip, bits))
-
-    # the appropriate bits in the wildcard should be set
-    self.assertEqual( (m.wildcards & bitmask) >> shift, 32-bits)
-
-    # reset to 0.0.0.0/0 results in full wildcard
-  setattr(m, attr, "0.0.0.0/0")
-  self.assertEquals(getattr(m, "get_"+attr)(), (None, 0), "get_%s for unset %s should return (None,0)" % (attr, attr))
-  self.assertTrue( ((m.wildcards & bitmask) >> shift) >= 32)
+class get_json(object):
+  
+  
+  def __init__(self, name):
+    self.name = name
     
-  #check the format is ipv4, ipv6 or else
-  #take out the ips that are ipv4
-  #take out the ips taht are ipv6
-  #emerge them into a new dict.
-  #then the user can use get_src_ip(ipv4) to get ipv4
-  #or get_src_ip(ipv6) to get ipv6
+  def load_json(self):
+    json_data = open(self.name)
+    json_object = json.load(json_data) #type: dict. 
+    return json_object
+     
+  def get_src_ip(self):
+    json_ip = json_object['address']['src']['network_layer']['ip_address']
+    #ip = json.dumps(json_ip, sort_keys=True, separators=(', ',': '))
+    ip_list = []
+    for key, value in json_ip.iteritems():
+      # convert ipv4 to ipv6
+      
+      # test if they are valid IPV4 Addresses
+      try:
+        socket.inet_aton(str(value))
+        ip_list.append(IPAddr(str(value)))
+        # legal
+      except socket.error:
+        print(value + 'is no an valid IP Address')
+        # Not legal
+      
+      
+      
+      if it is ip:
+        
+      else:
+        
+      
+      
+    for ipnet in ip:
+       
+    #check the format is ipv4, ipv6 or else
+    #take out the ips that are ipv4
+    #take out the ips taht are ipv6
+    #emerge them into a new dict.
+    #then the user can use get_src_ip(ipv4) to get ipv4
+    #or get_src_ip(ipv6) to get ipv6
+    
+    
+    
   
   
+  # read data_link_layer from object 
   
-
-
-# read data_link_layer from object 
-
 
 # read physical_layer from object 
 
