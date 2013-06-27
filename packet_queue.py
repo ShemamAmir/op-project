b75c703bdccfe76563ff502e9f4cded49d3c6c20
gordonyen <gordonyen@gmail.com>
2013-06-27 17:18:43 -0400

    Update packet_queue.py

--- packet_queue.py
+++ packet_queue.py
@@ -1,3 +1,4 @@
 #if there are multiple packets, and a queue becomes saturated then the QoS or priority score is used.
 #interact with json module
 #load balancing in multipath routing
+#http://en.wikipedia.org/wiki/Multipath_routing
