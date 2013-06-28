9ade0d5c9e22ccecd07650428932c0a5e595faa9
gordonyen <gordonyen@gmail.com>
2013-06-27 20:55:42 -0400

    Update packet_queue.py

--- packet_queue.py
+++ packet_queue.py
@@ -3,3 +3,4 @@
 #load balancing in multipath routing
 #http://en.wikipedia.org/wiki/Multipath_routing
 #load balancing in multifple path decision 
+#在乙太網協議中規定，同一區域網中的一台主機要和另一台主機進行直接通信，必須要知道目標主機的MAC地址。
