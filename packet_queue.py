dea97732289ee9c31dd1c423bec190398fa4c774
gordonyen <gordonyen@gmail.com>
2013-06-28 13:31:32 -0400

    Update packet_queue.py

--- packet_queue.py
+++ packet_queue.py
@@ -4,6 +4,10 @@
 #http://en.wikipedia.org/wiki/Multipath_routing
 #load balancing in multifple path decision 
 #在乙太網協議中規定，同一區域網中的一台主機要和另一台主機進行直接通信，必須要知道目標主機的MAC地址。
+#QoS: Quality of service is the ability to provide different priority to different applications, users,
+#     or data flows, or to guarantee a certain level of performance to a data flow. For example, a required
+#     bit rate, delay, jitter, packet dropping probability and/or bit error rate may be guaranteed. Quality
+#     of service guarantees are important if the network capacity is insufficient.
 
 class iplb (object):
   
