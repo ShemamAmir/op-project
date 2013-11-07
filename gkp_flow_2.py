4dddcc260a9f0dae3bbdc29d8cdab050721435a1
gordonyen <gordonyen@gmail.com>
2013-11-06 22:06:35 -0500

    Update gkp_flow_2.py

--- gkp_flow_2.py
+++ gkp_flow_2.py
@@ -73,37 +73,111 @@ def _handle_flowstats_received (event):
   stats = flow_stats_to_list(event.stats)
   log.debug("FlowStatsReceived from %s: %s",
   dpidToStr(event.connection.dpid), stats)
-  json_data=json.dumps({"flow_id": 0,
-  "time": "000-00-00T00:00:00Z",
-  "results": [
-    {
-      "destination_ip": "0.0.0.0",
-      "destination_port": 0000,
-      "source_ip": "0.0.0.0",
-      "source_port": 0,
-      "flow_bytes": 0,
-      "flow_packets": 0,
-      "port_bytes": 0,
-      "port_packets": 0
-    }
-  ]
+  json_data=json.dumps({
+    "flow_id": 0,
+    "time": "000-00-00T00:00:00Z",
+    "destination_ip": "0.0.0.0",
+    "destination_port": 0,
+    "source_ip": "0.0.0.0",
+    "source_port": 0,
+    "flow_bytes": 0,
+    "flow_packets": 0,
+    "port_bytes": 0,
+    "port_packets": 0
   })
   data_object = json.loads(json_data) 
-  data_object['port_id'] = event.connection.dpid
   data_object['time'] = timestamp_string()
+
+  #print data_object
   for f in event.stats:
-    for foo in data_object['results']:
-      foo['destination_ip'] = f.match.nw_dst
-      foo['destination_port'] = f.match.tp_dst
-      foo['source_ip'] = f.match.nw_src
-      foo['source_port'] = f.match.tp_src
-      foo['flow_bytes'] = flowbyte # not define yet
-      foo['flow_packets'] = flowpacket
-      foo['port_bytes'] = portbyte
-      foo['port_packets'] = portpacket# not define yet
-  print data_object
-  #for f in event.stats:
-  #  print stats
+    #print stats
+    #print "\n"
+    sVar=str(stats)
+    sVar=sVar.replace("IPAddr('","'IPAddr(")
+    sVar=sVar.replace("')",")'")
+    sVar=sVar.replace("'",'"')
+    preList=json.loads(sVar)
+    for foo in preList[0]:
+      data_object['destination_ip'] = foo['get_nw_dst']
+      data_object['destination_tcp'] = foo['get_tp_dst']
+      data_object['destination_port']=foo['get_dl_dst']
+      data_object['source_ip'] = foo['get_nw_src']
+      data_object['source_tcp'] = foo['get_tp_src']
+      data_object['source_port']=foo['get_dl_src']
+      data_object['flow_bytes'] = foo['byte_count'] # not define yet
+      data_object['flow_packets'] = foo['packet_count']
+      data_object['port_bytes'] = portbyte
+      data_object['port_packets'] = portpacket# not define yet
+    print data_object
+    ''' [
+    {
+        "packet_count": 11,
+        "hard_timeout": 30,
+        "byte_count": 1870,
+        "duration_sec": 3,
+        "actions": [
+            {
+                "max_len": 0,
+                "type": "OFPAT_OUTPUT",
+                "port": 2
+            }
+        ],
+        "duration_nsec": 754000000,
+        "priority": 65535,
+        "idle_timeout": 10,
+        "cookie": 0,
+        "table_id": 0,
+        "match": {
+            "dl_type": "IP",
+            "nw_dst": "IPADDR(10.0.0.2)",
+            "tp_src": 80,
+            "dl_src": "00:00:00:00:00:01",
+            "nw_proto": 6,
+            "nw_tos": 0,
+            "tp_dst": 55679,
+            "get_nw_src": "10.0.0.1/32",
+            "get_nw_dst": "10.0.0.2/32",
+            "dl_dst": "00:00:00:00:00:02",
+            "dl_vlan": 65535,
+            "nw_src": "IPADDR(10.0.0.1)",
+            "in_port": 1
+        }
+    },
+    {
+        "packet_count": 9,
+        "hard_timeout": 30,
+        "byte_count": 700,
+        "duration_sec": 3,
+        "actions": [
+            {
+                "max_len": 0,
+                "type": "OFPAT_OUTPUT",
+                "port": 1
+            }
+        ],
+        "duration_nsec": 714000000,
+        "priority": 65535,
+        "idle_timeout": 10,
+        "cookie": 0,
+        "table_id": 0,
+        "match": {
+            "dl_type": "IP",
+            "nw_dst": "IPADDR(10.0.0.1)",
+            "tp_src": 55679,
+            "dl_src": "00:00:00:00:00:02",
+            "nw_proto": 6,
+            "nw_tos": 0,
+            "tp_dst": 80,
+            "get_nw_src": "10.0.0.2/32",
+            "get_nw_dst": "10.0.0.1/32",
+            "dl_dst": "00:00:00:00:00:01",
+            "dl_vlan": 65535,
+            "nw_src": "IPADDR(10.0.0.2)",
+            "in_port": 2
+        }
+    }
+]
+'''
   #stats = flow_stats_to_list(event.stats)
   #flowbytes = 0
   #flows = 0
